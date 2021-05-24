884d15116974cec8f830446edb04faa0bba74f6c
chrisds7 <chrisds7@my.yorku.ca>
2021-05-24 12:01:59 -0400

    Add google auth

--- Winners Circuit/wc/src/pages/LogInPage/loginpage.js
+++ Winners Circuit/wc/src/pages/LogInPage/loginpage.js
@@ -1,20 +1,41 @@
 import React from "react";
 import './loginpage.css'
 import logo from '../logo.png'
+import GoogleLogin from "react-google-login";
+
+    const googleSuccess = (res) => {
+        console.log('Google sign in was a success');
+        console.log(res.profileObj);
+    }
+
+    const googleFailure = (err) => {
+        console.log('Google sign in was a failure');
+        console.log(err);
+    }
+
 
     const LogInPage = () => {
         return (
 
         <div id = 'logInBackground'>
             <div className = 'logInDiv'>
-                <img src={logo} id = 'logInLogo' alt = 'Logo'/>
-                <h1 id = 'logInHeader'>Log In</h1>
-                <form>
-                    <input type='text' className = 'inputFields' id = 'email' placeholder ='email'></input>
-                    <input type = 'text' className = 'inputFields' id = 'password' placeholder = 'password'></input>
-                </form>
-                <button id = 'signIn'> Sign In </button>
-                <p id = 'forgotPassword'> Forgot password </p>
+                <img src = {logo} className = 'logInLogo' alt = 'Logo'/>
+                <h1 className = 'logInHeader'>Log In and Connect with Google</h1>
+                <GoogleLogin
+                  clientId = {'828722406940-6lro5qhe5daopfll18gqjg9acbchhcos.apps.googleusercontent.com'}
+                  render = {(renderProps) => (
+                      <button
+                          className = 'googleButton'
+                          onClick = {renderProps.onClick}
+                          disabled = {renderProps.disabled}
+                      >
+                          Google Sign in
+                      </button>
+                  )}
+                  onSuccess = {googleSuccess}
+                  onFailure = {googleFailure}
+                  cookiePolicy={'single_host_origin'}
+                />
             </div>
         </div>
         )
