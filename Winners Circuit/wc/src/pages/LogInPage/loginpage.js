0a840589eb19d9f1e431a8d400739ddc08a0b4a8
DeathyVoid <will.iams.sapo@gmail.com>
2021-05-27 20:14:28 -0400

    Integrated frontend for Facebook login

--- Winners Circuit/wc/src/pages/LogInPage/loginpage.js
+++ Winners Circuit/wc/src/pages/LogInPage/loginpage.js
@@ -1,55 +1,84 @@
 import React from "react";
 import './loginpage.css'
 import logo from '../logo.png'
+import AppleLogin from "react-apple-login";
+import FacebookLogin from "react-facebook-login";
 import GoogleLogin from "react-google-login";
 
-    const googleSuccess = async (res) => {
-        console.log('Google sign in was a success');
-        console.log(res.profileObj);
+const googleSuccess = async (res) => {
+    console.log('Google sign in was a success');
+    console.log(res.profileObj);
 
-        const body = { google_id: res.profileObj.googleId, firstname: res.profileObj.givenName, lastname: res.profileObj.familyName, email: res.profileObj.email };
+    const body = {
+        google_id: res.profileObj.googleId,
+        firstname: res.profileObj.givenName,
+        lastname: res.profileObj.familyName,
+        email: res.profileObj.email
+    };
 
-        const response = await fetch("http://localhost:5000/signup", {
+    const response = await fetch("http://localhost:5000/signup", {
 
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(body)
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body)
 
-        });
+    });
 
-        console.log(response);
-    }
-
-    const googleFailure = (err) => {
-        console.log('Google sign in was a failure');
-        console.log(err);
-    }
-
-    const LogInPage = () => {
-        return (
-
-        <div id = 'logInBackground'>
-            <div className = 'logInDiv'>
-                <img src = {logo} className = 'logInLogo' alt = 'Logo'/>
-                <h1 className = 'logInHeader'>Log In and Connect with Google</h1>
-                <GoogleLogin
-                  clientId = {'828722406940-6lro5qhe5daopfll18gqjg9acbchhcos.apps.googleusercontent.com'}
-                  render = {(renderProps) => (
-                      <button
-                          className = 'googleButton'
-                          onClick = {renderProps.onClick}
-                          disabled = {renderProps.disabled}
-                      >
-                          Google Sign in
-                      </button>
-                  )}
-                  onSuccess = {googleSuccess}
-                  onFailure = {googleFailure}
-                  cookiePolicy={'single_host_origin'}
-                />
-            </div>
-        </div>
-        )
+    console.log(response);
 }
 
-    export default LogInPage;
+const facebookSuccess = async (res) => {
+
+    console.log(res);
+
+}
+
+const loginFailure = (err) => {
+    console.log('Log in was a failure');
+    console.log(err);
+}
+
+const responseFacebook = (response) => {
+    console.log(response);
+}
+
+const LogInPage = () => {
+
+    return (
+
+        <div id='logInBackground'>
+
+            <div className='logInDiv'>
+                <img src={logo} className='logInLogo' alt='Logo'/>
+                <h1 className='logInHeader'>Log In and Connect with Google</h1>
+
+                <GoogleLogin
+                    clientId={'828722406940-6lro5qhe5daopfll18gqjg9acbchhcos.apps.googleusercontent.com'}
+                    render={(renderProps) => (
+                        <button
+                            className='googleButton'
+                            onClick={renderProps.onClick}
+                            disabled={renderProps.disabled}
+                        >
+                            Google Sign in
+                        </button>
+                    )}
+                    onSuccess={googleSuccess}
+                    onFailure={loginFailure}
+                    cookiePolicy={'single_host_origin'}
+                />
+
+                <FacebookLogin
+                    appId="910935036418679"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    onClick={facebookSuccess}
+                    callback={responseFacebook}
+                />
+
+            </div>
+        </div>
+    )
+}
+
+export default LogInPage;
