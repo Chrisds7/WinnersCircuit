7ad26b063272d08b054ce4c584d6ba494c9afd1e
DeathyVoid <will.iams.sapo@gmail.com>
2021-06-17 17:41:15 -0400

    Added backend support for the registration form

--- Winners Circuit/wc/src/pages/RegisterComponent/RegisterComponent.js
+++ Winners Circuit/wc/src/pages/RegisterComponent/RegisterComponent.js
@@ -2,15 +2,44 @@ import React from "react";
 import "./RegisterComponent.css";
 import logo from '../logo.png';
 import { useHistory } from 'react-router-dom';
+import {registerAccount} from "../LoginComponent/LoginAccountRegister";
 
 class RegisterComponent extends React.Component {
-   // props;
 
     constructor(props) {
 
         super(props);
 
+        this.state = {
+
+            firstName: "",
+            lastName: "",
+            dateOfBirth: "",
+            heightFt: "",
+            heightIn: "",
+            school: "",
+            position: ""
+
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.sendData = this.sendData.bind(this);
+
     }
+
+    handleChange(event) {
+
+        const target = event.target;
+
+        const value = target.value;
+
+        const name = target.name;
+
+        this.setState({ [name]: value })
+
+    }
+
 
     render() {
 
@@ -21,23 +50,23 @@ class RegisterComponent extends React.Component {
                 <div className='signUpForm'>
                     <form>
                         <label htmlFor="fname">First Name: </label>
-                        <input type="text"/><br/>
+                        <input type="text" name = 'firstName' onChange = { this.handleChange }/><br/>
 
                         <label htmlFor="lname">Last Name: </label>
-                        <input type="text"/><br/>
+                        <input type="text" name = 'lastName' onChange = { this.handleChange }/><br/>
 
                         <label htmlFor="birthday">Date Of Birth: </label>
-                        <input type="date" id="birthday" name="birthday"/><br/>
+                        <input type="date" id="birthday" name = 'dateOfBirth' onChange = { this.handleChange }/><br/>
 
                         <label htmlFor="height-ft">Height: </label>
-                        <input type="text" className="height"/><span>ft</span>
-                        <input type="text" className="height"/><span>in</span><br/>
+                        <input type="value" className="height" name = "heightFt" onChange = { this.handleChange }/><span>ft</span>
+                        <input type="value" className="height" name = "heightIn" onChange = { this.handleChange }/><span>in</span><br/>
 
                         <label htmlFor="school">School: </label>
-                        <input type="text"/><br/>
+                        <input type="text" name = "school" onChange = { this.handleChange }/><br/>
 
                         <label htmlFor='position'>Position:</label>
-                        <select name='position'>
+                        <select name = "position" onChange = { this.handleChange }>
                             <option value='PG'>Point Guard</option>
                             <option value='SG'>Shooting Guard</option>
                             <option value='C'>Center</option>
@@ -45,7 +74,7 @@ class RegisterComponent extends React.Component {
                             <option value='PF'>Power Forward</option>
                         </select><br/>
 
-                        <button onClick={() => { this.sendData() }} >Begin Your Journey</button>
+                        <button onClick = { this.sendData } >Begin Your Journey</button>
                     </form>
                 </div>
             </div>
@@ -54,10 +83,36 @@ class RegisterComponent extends React.Component {
 
     }
 
-    sendData(){
+    async sendData(event) {
+
+        event.preventDefault();
+
         this.props.history.push('/feed');
 
-        //TODO post request down below in this function 
+        const body = {
+
+            // TODO: Change the userId to work with our authentication flow
+            userId: "12346",
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            dateOfBirth: this.state.dateOfBirth,
+            heightFt: this.state.heightFt,
+            heightIn: this.state.heightIn,
+            school: this.state.school,
+            position: this.state.position
+
+        }
+
+        console.log(body);
+
+        const response = await fetch("http://localhost:5000/register", {
+
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(body)
+
+        });
+
     }
 
 }
